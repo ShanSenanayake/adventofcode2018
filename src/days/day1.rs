c5e0e5930902ed6184f6c358dd32348170dc8ee6
Shan Senanayake <shan.senanayake@jayway.com>
2018-12-10 10:15:56 +0100

    day 1: second task complete

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,12 +1,30 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
-pub fn one() -> i32 {
+fn parse_input() -> Vec<i32>  {
     let file = File::open("input/day1_1.txt").expect("file not found");
-
     let input = BufReader::new(file).lines();
     input
-        .map(|line| line.unwrap().parse::<i32>().unwrap())
-        .sum()
+       .map(|line| line.unwrap().parse::<i32>().unwrap())
+       .collect::<Vec<i32>>()
+}
+
+pub fn one() -> i32 {
+    parse_input().into_iter().sum()
+}
+
+pub fn two() -> i32 {
+    let mut set = HashSet::new();
+    let mut acc = 0;
+    set.insert(acc);
+    parse_input()
+        .into_iter()
+        .cycle()
+        .find_map(|elem| {
+            acc += elem;
+            set.replace(acc)
+        })
+        .unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod days;
 
 fn main() {
     println!("Day1.1 result {:?}", days::day1::one());
+    println!("Day1.2 result {:?}", days::day1::two());
 }
